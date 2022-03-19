995ac2d47f9799fc76c6ecf64e22bafc343ef08e
Seif Omran <seifomran1324@gmail.com>
2022-03-19 20:47:51 +0200

    Config final

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 aws elasticbeanstalk create-application-version --application-name udagram-api --version-label test --source-bundle S3Bucket=seifsudagram,S3Key=Archive.zip
-eb init udagram-api
+eb init --region us-east-1 udagram-api
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_PORT=$POSTGRES_PORT POSTGRES_USERNAME=$POSTGRES_USERNAME
